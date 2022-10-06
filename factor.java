4f016e5e941c958d20952a84c0c41d332ef05d17
vuminh98 <vuminhtran027@gmail.com>
2022-10-06 10:24:19 +0700

    factor

---
+++ factor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class factor {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = input.nextInt();
+        System.out.println("Enter b: ");
+        b = input.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor");
+        }else {
+            while (a != b) {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
+}
